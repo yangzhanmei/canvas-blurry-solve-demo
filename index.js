4c2a431881a3ab93b2bf7336b330785fd8c9a61e
yangzhanmei <15809244435@163.com>
2017-07-13 02:54:01 +0800

    solve the blurry problem

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function draw() {
+    var getPixelRatio = function (context) {
+        var backingStore = context.backingStorePixelRatio ||
+            context.webkitBackingStorePixelRatio ||
+            context.mozBackingStorePixelRatio ||
+            context.msBackingStorePixelRatio ||
+            context.oBackingStorePixelRatio ||
+            context.backingStorePixelRatio || 1;
+        return (window.devicePixelRatio || 1) / backingStore;
+    };
+
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
-    ctx.font = "20px serif";
-    ctx.fillText("Hello world", 10, 50);
+    var ratio = getPixelRatio(ctx);
+
+    ctx.font = "30px serif";
+    ctx.fillText("Hello world", 10 * ratio, 50 * ratio);
 }
+
 draw();
